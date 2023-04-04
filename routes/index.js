a6d2cc6be3df00d7ccee54ecb4a19c65a5116274
kyathijagadeeswarp <123043429+kyathijagadeeswarp@users.noreply.github.com>
2023-04-04 18:58:43 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('shoe', { title: 'Search Results - Shoe' });
 });
 
 module.exports = router;
