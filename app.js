d49fd56d66be10f73483169a6fe9dbae97b4c648
kyathijagadeeswarp <123043429+kyathijagadeeswarp@users.noreply.github.com>
2023-04-04 19:44:55 -0500

    use a good message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var classRouter = require('./routes/class');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/class', classRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,15 +41,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-const express = require('express');
-const app = express();
-
-app.get('/shoes', (req, res) => {
-  // your code to handle the request goes here
-});
-
-// start the server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shoes', { title: 'Search Results - shoes' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
