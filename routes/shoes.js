f66032c3a5947704945367e9181608425e0cff43
kyathijagadeeswarp <123043429+kyathijagadeeswarp@users.noreply.github.com>
2023-04-04 20:04:27 -0500

    Create shoes.js

---
+++ routes/shoes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('shoes', { title: 'Search Results - Shoes' });
+});
+
+module.exports = router;
