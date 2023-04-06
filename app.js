f5c6c29b0cf64eabe7897eb5e3dec410387719f2
kyathijagadeeswarp <123043429+kyathijagadeeswarp@users.noreply.github.com>
2023-04-05 20:03:36 -0500

    use a good message

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var shoeRouter = require('./routes/shoe');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -21,9 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/shoe', shoeRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
-var shoesRouter = require('./routes/shoes');
-app.use('/shoes', shoesRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/shoe.js
+++ routes/shoe.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('shoes', { title: 'Search Results - Shoes' });
-   
-  });
-  
+  res.render('shoe', { title: 'Search Results Shoe' });
+});
 
 module.exports = router;
